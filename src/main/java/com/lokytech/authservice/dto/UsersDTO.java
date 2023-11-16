25108f14a131f5c1f0272aecb0d69597bee10ca9
lokytech5 <lokytech5@gmail.com>
2023-11-16 17:12:25 +0100

    Added required dto for users, role and loginRequest

---
+++ src/main/java/com/lokytech/authservice/dto/UsersDTO.java
@@ -0,0 +1,71 @@
+package com.lokytech.authservice.dto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class UsersDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime registrationDate;
+    private LocalDateTime lastLogin;
+
+    private Set<RoleDTO> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleDTO> roles) {
+        this.roles = roles;
+    }
+}
